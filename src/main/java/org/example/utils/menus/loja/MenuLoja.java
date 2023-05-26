a9f15908e859716865eba9d03b08a511d1bc6272
Joao Victor Marinho <joaovm048@gmail.com>
2023-05-25 22:46:04 -0300

    Marinho: inicio fachada e serviço de

--- src/main/java/org/example/utils/menus/loja/MenuLoja.java
+++ src/main/java/org/example/utils/menus/loja/MenuLoja.java
@@ -3,9 +3,10 @@ package org.example.utils.menus.loja;
 public class MenuLoja {
     public static void initialPage(){
         System.out.println("Escolha a opção");
-        System.out.println("1 - visualizar lojas");
-        System.out.println("2 - visualizar produtos de uma loja especifica");
-        System.out.println("3 - visualizar todos os produtos");
+        System.out.println("1 - visualizar produtos em estoque");
+        System.out.println("2 - adicionar produtos ao estoque");
+        System.out.println("3 - remover produtos do estoque");
+        System.out.println("4 - atualizar dados de produtos");
         System.out.println("8 - Fazer logout");
         System.out.println("9 - Fechar sistema");
     }
